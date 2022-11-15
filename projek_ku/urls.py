cb0b1abf4cb75e9d1cb40a2b04714827cea5d627
irmansyah38 <irmansya146@gmail.com>
2022-11-15 17:27:54 +0700

    tinggal log in dan sign_up

--- projek_ku/urls.py
+++ projek_ku/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from website.views import halaman1, login , gbk , sign_up
+from website.views import halaman1 , gbk ,login ,sign_up
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('halaman1/', halaman1),
-    path('login/', login),
-    path('gbk/', gbk),
-    path('sign_up/', sign_up),
+    # path('admin/', admin.site.urls),
+    path('', halaman1,name='halaman1'),
+    path('gbk/', gbk,name='gbk'),
+    path('login/', login,name='login'),
+    path('sign_up/', sign_up,name='sign_up'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,20 +1,23 @@
-
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def halaman1(request):
-    return render(request,'depan.html')
-
-def login(request):
-    return render(request,'login.html')
+    return render(request,'halaman1.html')
 
 def gbk(request):
     return render(request,'gbk.html')
 
+def login(request):
+    context = {
+
+    }
+    if request.method == 'POST':
+        context['user_name'] = str(request.POST['user_name'])
+        context['password'] = str(request.POST['password'])
+
+    return render(request,'login.html',context)
+
 def sign_up(request):
     return render(request,'sign_up.html')
-
-# def login(request):
-   
-   
